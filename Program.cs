7570719948f5d85458f2f08efbedc3281b3b3a2a
SyntaxNate <new-shutter.nbt@gmail.com>
2025-11-24 22:47:23 -0500

    Initial C# console project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Ask name
+Console.Write("Enter Your Name: ");
+string name = Console.ReadLine();
+
+//Ask age 
+Console.Write("Enter You Age: ");
+int age = int.Parse(Console.ReadLine());
+
+//Calculate the year you will turn 40
+int currentYear = DateTime.Now.Year;
+int yearAt40 = currentYear + (40 - age);
+
+Console.WriteLine();
+Console.WriteLine($"Hey {name}, you will turn 40 in the year {yearAt40}!");
+
